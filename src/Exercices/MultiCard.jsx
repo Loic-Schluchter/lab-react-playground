95aaeccb930be904b9c30a965c9aa8229cf9ff7f
Loïc Schluchter <loicschluchterdev@gmail.com>
2025-10-14 21:33:41 +0200

    Update MultiCard.jsx

--- src/Exercices/MultiCard.jsx
+++ src/Exercices/MultiCard.jsx
@@ -1,49 +1,43 @@
-import React from 'react'
-import Card from './components/Card'
-import { useRef, useEffect } from 'react'
+import React from "react";
+import Card from "./components/Card";
+import { useRef, useEffect } from "react";
 
 export default function MultiCard() {
+  const card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const cardRefs = useRef([]);
 
-    const card = [1,2,3,4,5,6,7,8,9,10];
-    const cardRefs = useRef([]);
-  
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    });
 
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("visible");
-                    observer.unobserve(entry.target);
-                } 
-            });
-        });
-        
-        cardRefs.current.forEach((ref) => {
-            if (ref) observer.observe(ref);
-        });
+    cardRefs.current.forEach((ref) => {
+      if (ref) observer.observe(ref);
+    });
 
-        return () => {
-            cardRefs.current.forEach(ref => {
-                if (ref) observer.unobserve(ref);
-            });
-        };
-
-    }, []);
-
-
+    return () => {
+      cardRefs.current.forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, []);
 
   return (
     <div className="flex flex-col gap-8">
-        {card.map((_, index) => (
-            <div 
-                key={index}
-                ref={(el) => cardRefs.current[index] = el}
-                className="cardBoard"
-                >
-                    <Card />
-            </div>
-        ))}
-        
+      {card.map((_, index) => (
+        <div
+          key={index}
+          ref={(el) => (cardRefs.current[index] = el)}
+          className="cardBoard"
+        >
+          <Card />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
